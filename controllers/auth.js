2ea62b2126ed308b7aa9fda47e3ec5e5a5483719
Gary Dillard <thomas.dillard.studios@gmail.com>
2017-12-14 13:37:31 -0700

    mostly Ryan's solution

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,16 @@
+const requireSignIn = require('../services/passport').requireSignIn;
+const getTokenForUser = require('../services/token');
+
 const signUp = (req, res) => {
   // create a new user and return a valid JWT token to the client
 };
 
 const signIn = (req, res) => {
-  // generate a JWT token if the username/password is valid
+  console.log(requireSignIn + '');
+  res.send({ token: getTokenForUser(req.user) });
 };
 
 module.exports = (app) => {
   app.post('/signup', signUp);
-  app.post('/signin', signIn);
+  app.post('/login', requireSignIn, signIn);
 };
--- models/user.js
+++ models/user.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const UserSchema = mongoose.Schema({
-  email: {
+  username: {
     type: String,
     unique: true,
     lowercase: true,
     required: true,
-    validate: {
-      validator: (email) => {
-        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-      }
-    }
   },
   password: {
     type: String,
@@ -21,10 +17,30 @@ const UserSchema = mongoose.Schema({
 UserSchema.pre('save', function(next) {
   // this is where we will hash the user's password
   // generate the salt and hash the password using bcrypt
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    // hash password
+    bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 UserSchema.methods.checkPassword = function(potentialPassword, cb) {
   // use bcrypt to compare the potentialPassword with the user's password
+  console.log('>>>>>>>>>>> hitting me', potentialPassword, this.password);
+  bcrypt
+    .compare(potentialPassword, this.password, (err, result) => {
+      if (err) cb(err);
+      cb(null, result);
+    });
+    // .then(result => {
+    //   console.log(result);
+    //   cb(null, result)
+    // })
+    // .catch(err => cb(err));
 };
 
 module.exports = UserSchema;
